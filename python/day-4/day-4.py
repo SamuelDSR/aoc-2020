381db37c2a557842d41aa0244c3bc7f5cea029c6
Shihe LONG <slong.veepee.com>
2020-12-05 07:36:48 +0100

    day 3/4/5 solutions

---
+++ python/day-4/day-4.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+def load_input(path):
+    with Path(path).open('r') as f:
+        lines = f.read()
+    passports = lines.split("\n\n")
+    passports = [
+        dict(
+            (t.split(":")[0], t.split(":")[1])
+            for t in passport.strip().replace("\n", " ").split(" ")
+        )
+        for passport in passports
+    ]
+    return passports
+
+def valid_passports_1(passports):
+    ask_keys = set(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
+    valid = 0
+    for p in passports:
+        if len(ask_keys - set(p.keys())) == 0:
+            valid += 1
+    return valid
+
+def valid_passports_2(passports):
+    def _valid_range(value, minv, maxv):
+        return value is not None and minv <= int(value) <= maxv
+
+    valid = 0
+    for p in passports:
+        if not _valid_range(p.get("byr"), 1920, 2002):
+            continue
+        if not _valid_range(p.get("iyr"), 2010, 2020):
+            continue
+        if not _valid_range(p.get("eyr"), 2020, 2030):
+            continue
+        hgt = p.get("hgt")
+        if hgt is None:
+            continue
+        if hgt.endswith("cm"):
+            if not _valid_range(int(hgt[:-2]), 150, 193):
+                continue
+        elif hgt.endswith("in"):
+            if not _valid_range(int(hgt[:-2]), 59, 76):
+                continue
+        else:
+            continue
+
+        hcl = p.get("hcl")
+        if hcl is None or (not hcl.startswith("#")) or (not len(hcl) == 7):
+            continue
+        elements = set(hcl[1:])
+        if len(elements - set([str(x) for x in range(10)] + ['a', 'b', 'c', 'd', 'e', 'f'])) != 0:
+            continue
+
+        ecl = p.get("ecl")
+        if ecl is None or not (ecl in set(["amb", "brn", "blu", "gry", "grn", "hzl", "oth"])):
+            continue
+
+        pid = p.get("pid")
+        if pid is None or len(pid) != 9:
+            continue
+        try:
+            int(pid)
+        except:
+            continue
+        valid += 1
+    return valid
+
+
+if __name__ == '__main__':
+    passports = load_input("day-4-input.txt")
+
+    print(valid_passports_1(passports))
+    print(valid_passports_2(passports))
+
+    test_passports = load_input("test-input.txt")
+    print(valid_passports_2(test_passports))
+#
+    test_passports = load_input("test-input-2.txt")
+    print(valid_passports_2(test_passports))
---
+++ python/day-5/day-5.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+def seat_to_int(seat_code):
+    seat_row = int(seat_code[:7].replace("F", "0").replace("B", "1"), 2)
+    seat_col = int(seat_code[7:].replace("R", "1").replace("L", "0"), 2)
+    return seat_row*8 + seat_col
+
+def load_input(path):
+    with Path(path).open('r') as f:
+        lines = f.readlines()
+    seat_codes = [
+        seat_to_int(ln.strip()) for ln in lines
+    ]
+    return seat_codes
+
+def part_1(seat_codes):
+    return max(seat_codes)
+
+def part_2(seat_codes):
+    lower = min(seat_codes)
+    upper = max(seat_codes)
+    return list((set(range(lower, upper+1)) - set(seat_codes)))[0]
+
+if __name__ == '__main__':
+    seat_codes = load_input("day-5-input.txt")
+    print(part_1(seat_codes))
+    print(part_2(seat_codes))
+
